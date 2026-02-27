f30de3fc81a4fbe6c8824169e7e062deb11d0ac7
muhammad sohail <abbasssohail44@gmail.com>
2026-02-28 04:14:55 +0500

    dummy

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -14,8 +14,8 @@ const router = Router();
 // Public routes
 router.post('/register', register);
 router.post('/login', login);
-
-// Protected routes
+//d
+// Protected routesss
 router.get('/me', protect, getMe);
 router.put('/profile', protect, updateProfile);
 router.put('/password', protect, changePassword);
