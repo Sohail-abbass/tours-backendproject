76b0e8a4f495fe3fd94e18a2ee056c94ed80f081
muhammad sohail <abbasssohail44@gmail.com>
2026-03-05 01:52:35 +0500

    something changes

--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Tour, ITour } from './Tour';
-export { default as Package, IPackage } from './Package';
-export { default as Destination, IDestination } from './Destination';
-export { default as Booking, IBooking } from './Booking';
-export { default as User, IUser } from './User';
-
